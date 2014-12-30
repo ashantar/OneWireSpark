6e5d9d2e9de6b0ed8bc6ab1fcc4dadc3bea88ebf
ashantar <ashantar@hotmail.com>
2014-12-30 23:36:43 +0100

    Update OneWire.h

--- firmware/OneWire.h
+++ firmware/OneWire.h
@@ -33,7 +33,6 @@
 //In file included from OneWire/OneWire.cpp:131:0:
 //OneWire/OneWire.h:31:0: warning: "FALSE" redefined [enabled by default]
 //FIXED?
-#define FALSE 0
 #define FALSE (0)
 #define TRUE  (1)
 
